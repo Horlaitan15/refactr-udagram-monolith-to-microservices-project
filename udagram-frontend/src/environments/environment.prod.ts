5236d5304ff0cae3c21169daa12bd3e38be5dbfa
Horlaitan15 <ajanihorlaitan@gmail.com>
2022-07-25 17:28:29 +0100

    Still on it.

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a248193fcf07d42bcb561431d7d6720d-502597003.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
